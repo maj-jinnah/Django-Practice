e07448d07bc90969de9f9606178e202c3101dc1d
maj-jinnah <maj.jinnah1999@gmail.com>
2023-12-14 23:42:37 +0600

    add module 15.5

---
+++ Module 15.5/Musicians_Directory/album/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from musician.models import Musician
+
+# Create your models here.
+class Album(models.Model):
+    album_name = models.CharField(max_length=100)
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    release_date = models.DateField()
+    album_ratings = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return self.album_name
